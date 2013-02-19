eeb2c25c6e9eb1a3393b94dbcc9051e4f3b0dd2f
Camilo Lopez <camilo@camilolopez.com>
2013-02-19 00:27:59 -0500

    First steps toward histogram support

--- README.md
+++
@@ -1,4 +0,0 @@
-go-opencv-mirror
-================
-
-Shamelessly stolen from http://code.google.com/p/go-opencv/
--- cxcore_test.go
+++ cxcore_test.go
@@ -8,3 +8,15 @@ func TestLoadImage2(t *testing.T) {
 	// t.Errorf("aaa")
 }
 
+func TestCreateHist(t *testing.T) {
+	img := LoadImage("./iguana.jpg")
+
+	if img == nil {
+		t.Error("Could not load image")
+	}
+
+	ranges := []float64{0, 255}
+	h := CreateHist(1, 256, CV_HIST_ARRAY, ranges, true)
+	defer h.Release()
+	h.Clear()
+}
